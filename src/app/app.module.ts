28cf273a2edfffa43e1a23b2869a193ff09c7e4c
Krisuuuuuu <krisu1997@onet.pl>
2021-03-18 16:46:27 +0100

    Added toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageModule } from './components/main-page/main-page.module';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
+import { ToolbarComponent } from './components/user-panel/toolbar/toolbar.component';
+import { 'toolbar'Module } from './components/user-panel/toolbar/'toolbar'.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserPanelComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MainPageModule
+    MainPageModule,
+    'toolbar'Module
   ],
   providers: [],
   bootstrap: [AppComponent]
