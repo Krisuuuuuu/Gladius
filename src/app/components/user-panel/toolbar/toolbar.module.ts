3c766a0c0f3d414243843b98e9960233bbd46761
Krisuuuuuu <krisu1997@onet.pl>
2021-03-24 18:02:26 +0100

    Added few mixins

--- src/app/components/user-panel/toolbar/toolbar.module.ts
+++ src/app/components/user-panel/toolbar/toolbar.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ToolbarComponent } from './toolbar.component';
 
