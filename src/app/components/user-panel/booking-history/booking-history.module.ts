69210f79759fb6301613d83863a93b17d9daeff1
Krisuuuuuu <krisu1997@onet.pl>
2021-03-29 15:27:38 +0200

    Exported table to booking-table component

--- src/app/components/user-panel/booking-history/booking-history.module.ts
+++ src/app/components/user-panel/booking-history/booking-history.module.ts
@@ -4,11 +4,12 @@ import { BookingHistoryComponent } from './booking-history.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { BookingTableComponent } from './booking-table/booking-table.component';
 
 
 
 @NgModule({
-  declarations: [BookingHistoryComponent],
+  declarations: [BookingHistoryComponent, BookingTableComponent],
   imports: [
     CommonModule,
     MatTabsModule,
---
+++ src/app/components/user-panel/booking-history/booking-table/booking-table.component.ts
@@ -0,0 +1,58 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+export interface TableElement {
+  no: number,
+  activityName: string,
+  activityDate: string,
+  activityDay: string,
+  activityHour: string,
+  bookingDate: string,
+  gymName: string,
+  status: string
+}
+
+@Component({
+  selector: 'app-booking-table',
+  templateUrl: './booking-table.component.html',
+  styleUrls: ['./booking-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class BookingTableComponent implements OnInit {
+  columnsToDisplay: string[] = ["No.", "Activity Date", "Activity Day", "Activity Hour", "Activity Name", "Booking Status"];
+  expandedElement: TableElement | null;
+  data: TableElement[] = [
+    {
+      no: 1,
+      activityName: "Example Activity",
+      activityDate: "00-00-0000",
+      activityDay: "Monday",
+      activityHour: "9.00",
+      bookingDate: "00-00-0000",
+      gymName: "Example Gym",
+      status: "Active"
+    },
+    {
+      no: 2,
+      activityName: "Example Activity",
+      activityDate: "00-00-0000",
+      activityDay: "Monday",
+      activityHour: "9.00",
+      bookingDate: "00-00-0000",
+      gymName: "Example Gym",
+      status: "Cancelled"
+    }
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
