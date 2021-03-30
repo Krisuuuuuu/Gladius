c3f33c856a782cae1437a377c818f93b5381fe61
Krisuuuuuu <krisu1997@onet.pl>
2021-03-30 13:16:06 +0200

    Exported calendar headers to separated component

--- src/app/components/user-panel/calendar/calendar.module.ts
+++ src/app/components/user-panel/calendar/calendar.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 import { CalendarComponent } from './calendar.component';
+import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
 
 
 
 @NgModule({
-  declarations: [CalendarComponent],
+  declarations: [CalendarComponent, CalendarHeaderComponent],
   imports: [
     CommonModule,
     MatIconModule
