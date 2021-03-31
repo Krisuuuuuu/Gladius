ba6f41fd1564487914ad777650c36fcadb9a050f
Krisuuuuuu <krisu1997@onet.pl>
2021-03-31 08:50:08 +0200

    Refactored booking-history

--- src/app/components/user-panel/booking-history/booking-history.module.ts
+++ src/app/components/user-panel/booking-history/booking-history.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BookingHistoryComponent } from './booking-history.component';
+import { BookingHistoryComponent } from './booking-history/booking-history.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -9,7 +9,10 @@ import { BookingTableComponent } from './booking-table/booking-table.component';
 
 
 @NgModule({
-  declarations: [BookingHistoryComponent, BookingTableComponent],
+  declarations: [
+    BookingHistoryComponent,
+    BookingTableComponent
+  ],
   imports: [
     CommonModule,
     MatTabsModule,
