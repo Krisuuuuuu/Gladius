d5f397cfb4c0500c0e9040dacb9f30fc187766f9
Krisuuuuuu <krisu1997@onet.pl>
2021-04-28 18:08:03 +0200

    Added booking history selectors

---
+++ src/app/state+/selectors/BookingHistory.selectors.ts
@@ -0,0 +1,15 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { IBookingHistoryState } from "../model/IBookingHistoryState";
+
+const selectBookingHistoryState = createFeatureSelector<IBookingHistoryState>(
+  'bookingHistory'
+);
+
+const selectBookings = createSelector(
+  selectBookingHistoryState,
+  (state: IBookingHistoryState) => state.bookings
+);
+
+export const BookingHistorySelectors = {
+  selectBookings
+}
