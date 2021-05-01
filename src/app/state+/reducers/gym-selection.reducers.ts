f9058b4e7dc4bfb9446b3d3db781d4c7cf909803
Krisuuuuuu <krisu1997@onet.pl>
2021-05-01 11:53:52 +0200

    Added reducers related with gym selection

---
+++ src/app/state+/reducers/gym-selection.reducers.ts
@@ -0,0 +1,33 @@
+import { Action, createReducer, on } from "@ngrx/store";
+import { GymSelectionActions } from "../actions/gym-selection.actions";
+import { IGymSelectionState, InitialGymSelectionState } from "../model/IGymSelection";
+
+const reducer = createReducer(
+  InitialGymSelectionState,
+  on(GymSelectionActions.loadGyms, (state: IGymSelectionState) => {
+    return {
+      ...state,
+      areGymsReceiving: true
+    };
+  }),
+  on(GymSelectionActions.gymsReceived, (state: IGymSelectionState, { gyms }) => {
+    return {
+      ...state,
+      areGymsReceiving: false,
+      gyms: gyms,
+    };
+  }),
+  on(GymSelectionActions.currentGymChanged, (state: IGymSelectionState, { gym }) => {
+    return {
+      ...state,
+      currentGym: {...gym}
+    };
+  })
+)
+
+export function gymSelectionReducer(
+  state: IGymSelectionState | undefined,
+  action: Action
+): IGymSelectionState {
+  return reducer(state, action);
+};
