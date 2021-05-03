d2e4893776ea51fb723d58c66223d40527032f81
Krisuuuuuu <krisu1997@onet.pl>
2021-05-03 12:33:58 +0200

    Added logic to gym selection component

--- src/app/components/user-panel/gym-selection/gym-card/gym-card.component.ts
+++ src/app/components/user-panel/gym-selection/gym-card/gym-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IGym } from 'src/app/model/gym-selection/IGym';
 
 @Component({
   selector: 'app-gym-card',
   templateUrl: './gym-card.component.html',
   styleUrls: ['./gym-card.component.scss']
 })
-export class GymCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class GymCardComponent {
+  @Input() gym: IGym;
 }
--- src/app/components/user-panel/gym-selection/gym-selection/gym-selection.component.ts
+++ src/app/components/user-panel/gym-selection/gym-selection/gym-selection.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IGym } from 'src/app/model/gym-selection/IGym';
 import { AppDataService } from 'src/app/services/app-data.service';
+import { GymSelectionActions } from 'src/app/state+/actions/gym-selection.actions';
+import { GymSelectionSelectors } from 'src/app/state+/selectors/gym-selection.selectors';
 
 @Component({
   selector: 'app-gym-selection',
@@ -8,9 +12,25 @@ import { AppDataService } from 'src/app/services/app-data.service';
 })
 export class GymSelectionComponent implements OnInit {
 
-  constructor(private appDataService: AppDataService) { }
+  gyms: Array<IGym>;
+
+  constructor(private store: Store<any>) { }
 
   ngOnInit(): void {
+    this.store.select(GymSelectionSelectors.selectGyms).subscribe(
+      gyms => this.gyms = gyms
+    );
+    this.getGyms();
 
+    if(this.gyms !== undefined && this.gyms.length > 0)
+      this.changeCurrentGym(this.gyms[0]);
+  }
+
+  changeCurrentGym(gym: IGym): void {
+    this.store.dispatch(GymSelectionActions.currentGymChanged({ gym: gym }));
+  }
+
+  private getGyms(): void {
+    this.store.dispatch(GymSelectionActions.loadGyms({ email: "mock" }));
   }
 }
